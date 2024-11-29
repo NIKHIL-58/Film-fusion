7548c9e55eeca3244aba7dae8911090dbb600315
Nikhil Dubey <94116815+NIKHIL-58@users.noreply.github.com>
2024-11-29 09:33:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     MONGO_URI = 'mongodb://localhost:27017/MovieDB'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads') #os.getcwd is get current working directory.
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
-    API_KEY = 'dd39691a'
-    YOUTUBE_API_KEY = 'AIzaSyAHX3vfeBZ51GkPTHV6TWWK4y4NIoPwOxY'
+    API_KEY = '@@@@@@@@@@@'
+    YOUTUBE_API_KEY = '@@@@@@@@@@@@@@@@@@@@@@@@@@@'
